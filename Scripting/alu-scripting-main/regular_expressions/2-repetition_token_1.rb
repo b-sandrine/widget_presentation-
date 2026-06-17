8364e47bcb957b5503f83f8c79d07252af45fc0c
sandrine dushimimana <76705091+b-sandrine@users.noreply.github.com>
2026-06-17 12:31:16 +0200

    Delete Scripting directory

--- Scripting/alu-scripting-main/regular_expressions/2-repetition_token_1.rb
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env ruby
-# A regular pattern that matches above cases
-puts ARGV[0].scan(/hb?tn/).join
--- Scripting/alu-scripting/regular_expressions/1-repetition_token_0.rb
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env ruby
-# A regular pattern that matches above cases
-puts ARGV[0].scan(/hbt{2,5}n/).join
